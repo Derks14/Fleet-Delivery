57618a4ff7cd7b608d7a6e4f4a2393ff4f9b59ba
4teen <meet4teen@gmail.com>
2020-02-04 02:29:41 +0000

    django heroku added

--- fleet/settings.py
+++ fleet/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,16 +21,16 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'kegdmbnxsl_5)5987h!t0__%3)5@%zw-a7ddv+&c!f24v3vjoa'
+SECRET_KEY = 'kegdmbnxsl_5)5987h!t0__%3)5@%zw-a7ddv+&c!f24v3vjoa'
 
-SECRET_KEY = os.environ.get('SECRET_KEY')
+# SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 
-ALLOWED_HOSTS = ['fleetglobal.herokuapp.com']
+ALLOWED_HOSTS = ['fleetglobal.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -126,3 +127,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
+django_heroku.settings(locals())
