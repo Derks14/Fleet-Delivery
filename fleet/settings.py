6a17387b75c5e77f63989740c3c99f952416ab7b
4teen <meet4teen@gmail.com>
2020-02-04 01:31:16 +0000

    updated settings

--- fleet/settings.py
+++ fleet/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = True
 
 
-ALLOWED_HOSTS = ['https://fleetglobal.herokuapp.com']
+ALLOWED_HOSTS = ['https://fleetglobal.herokuapp.com/']
 
 
 # Application definition
